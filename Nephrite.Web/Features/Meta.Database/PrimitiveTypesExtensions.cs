f4e272caed759dc459427867e732bc5efdc0dcf3
ipmen <aleksey.tegay@gmail.com>
2013-10-29 14:04:15 +0500

    Докамитил синхронизацию

---
+++ Nephrite.Web/Features/Meta.Database/PrimitiveTypesExtensions.cs
@@ -0,0 +1,71 @@
+﻿using Nephrite.Meta.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nephrite.Meta;
+
+namespace Nephrite.Meta
+{
+	public partial class MetaDecimalType
+	{
+		public override string GetDBType(IDBScript script)
+		{
+			return script.GetDecimalType(Precision, Scale);
+		}
+	}
+
+	public partial class MetaStringType
+	{
+		public override string GetDBType(IDBScript script)
+		{
+			return script.GetStringType(Length);
+		}
+	}
+
+	public partial class MetaDateTimeType
+	{
+		public override string GetDBType(IDBScript script)
+		{
+			return script.GetDateTimeType();
+		}
+	}
+	public partial class MetaZoneDateTimeType
+	{
+		public override string GetDBType(IDBScript script)
+		{
+			return script.GetZoneDateTimeType();
+		}
+	}
+
+	public partial class MetaDateType
+	{
+		public override string GetDBType(IDBScript script)
+		{
+			return script.GetDateType();
+		}
+	}
+
+	public partial class MetaIntType
+	{
+		public override string GetDBType(IDBScript script)
+		{
+			return script.GetIntType();
+		}
+	}
+
+	public partial class MetaLongType
+	{
+		public override string GetDBType(IDBScript script)
+		{
+			return script.GetLongType();
+		}
+	}
+	public partial class MetaGuidType
+	{
+		public override string GetDBType(IDBScript script)
+		{
+			return script.GetGuidType();
+		}
+	}
+}
---
+++ TestSchema/TestSchema.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nephrite.Meta;
+using Nephrite.Meta.Database;
+using Nephrite.Web;
+using System.Data.Linq;
+
+namespace TestSchema
+{
+	[TestClass]
+	public class TestSchema
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			string cs = "Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=servants;Data Source=TOSHIBA-TOSH\\SQL2008";
+			Base.Model = new DataContext(cs);
+			var s = MetaSolution.Load();
+			var schema = new Schema();
+			foreach (var d in s.Classes)
+			{
+				schema.Generate(d);
+			}
+			foreach (var table in schema.Tables)
+			{
+ 				table.
+			}
+		}
+	}
+}
