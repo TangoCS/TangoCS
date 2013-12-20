d86593b5c699f09424cc99f9b7b7c77d59577275
Vadim Lavrentyev <lubein@gmail.com>
2013-12-20 19:51:47 +0400

    Починил кривые файлы

---
+++ Nephrite.Web/RussianStrings/RussianNumber.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nephrite.Web.RussianStrings
+{
+	public static partial class RussianExtensions
+	{
+		
+	}
+}
