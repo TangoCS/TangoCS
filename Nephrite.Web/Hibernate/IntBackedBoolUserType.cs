2fc0f7b8a3abb961c466db41e595a1691765d5bd
ipmen <aleksey.tegay@gmail.com>
2014-01-28 14:50:41 +0500

    Добавил юзер типы

---
+++ Nephrite.Web/Hibernate/IntBackedBoolUserType.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Data;
+using NHibernate;
+using NHibernate.SqlTypes;
+using NHibernate.UserTypes;
+
+namespace Nephrite.Web.Hibernate
+{
+	public class IntBackedBoolUserType : IUserType
+	{
+		public new bool Equals(object x, object y)
+		{
+			if (ReferenceEquals(x, y)) return true;
+
+			if (x == null || y == null) return false;
+
+			return x.Equals(y);
+		}
+
+		public int GetHashCode(object x)
+		{
+			return x.GetHashCode();
+		}
+
+		public object DeepCopy(object value)
+		{
+			return value;
+		}
+
+		public object Replace(object original, object target, object owner)
+		{
+			return original;
+		}
+
+		public object Assemble(object cached, object owner)
+		{
+			return cached;
+		}
+
+		public object Disassemble(object value)
+		{
+			return value;
+		}
+
+		public SqlType[] SqlTypes
+		{
+			get { return new[] { new SqlType(DbType.Int32) }; }
+		}
+
+		public Type ReturnedType
+		{
+			get { return typeof(bool); }
+		}
+
+		public bool IsMutable
+		{
+			get { return false; }
+		}
+
+		public void NullSafeSet(IDbCommand cmd, object value, int index)
+		{
+			var val = !((bool)value) ? 0 : 1;
+			NHibernateUtil.Int32.NullSafeSet(cmd, val, index);
+		}
+
+		public object NullSafeGet(IDataReader rs, string[] names, object owner)
+		{
+			return NHibernateUtil.Boolean.NullSafeGet(rs, names[0]);
+		}
+	}
+}
---
+++ Nephrite.Web/Hibernate/StringBackedGuidUserType.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data;
+using NHibernate;
+using NHibernate.SqlTypes;
+using NHibernate.UserTypes;
+
+namespace Nephrite.Web.Hibernate
+{
+	public class StringBackedGuidUserType : IUserType
+	{
+		public bool Equals(object x, object y)
+		{
+			return true;
+		}
+
+		public int GetHashCode(object x)
+		{
+			return x.GetHashCode();
+		}
+
+		public object DeepCopy(object value)
+		{
+			return value;
+		}
+
+		public object Replace(object original, object target, object owner)
+		{
+			return original;
+		}
+
+		public object Assemble(object cached, object owner)
+		{
+			return cached;
+		}
+
+		public object Disassemble(object value)
+		{
+			return value;
+		}
+
+		public SqlType[] SqlTypes
+		{
+			get { return new[] { SqlTypeFactory.GetString(36) }; }
+		}
+
+		public Type ReturnedType
+		{
+			get { return typeof(Guid?); }
+		}
+
+		public bool IsMutable
+		{
+			get { return false; }
+		}
+
+		public void NullSafeSet(IDbCommand cmd, object value, int index)
+		{
+
+			NHibernateUtil.String.NullSafeSet(cmd, value == null ? null : value.ToString(), index);
+		}
+
+		public object NullSafeGet(IDataReader rs, string[] names, object owner)
+		{
+			return NHibernateUtil.Guid.NullSafeGet(rs, names[0]);
+		}
+	}
+}
