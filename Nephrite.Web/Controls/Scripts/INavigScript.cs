0b2df60352753a829fb9c3e8945e18a596744c0d
Aleksey <aleksey.tegay@live.ru>
2014-02-17 14:57:55 +0500

    Добавил NavigScripts

---
+++ Nephrite.Web/Controls/Scripts/INavigScript.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nephrite.Web.Controls.Scripts
+{
+    public interface INavigScript
+    {
+        string Menu { get; }
+    }
+}
