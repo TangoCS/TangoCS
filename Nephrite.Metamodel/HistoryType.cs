b90f1dcdc437e99e8f8d108d104e0a88adcd4ca0
Aleksey <aleksey.tegay@live.ru>
2014-03-13 15:26:05 +0500

    перенос шаблонов

---
+++ Nephrite.Metamodel/HistoryType.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nephrite.Web;
+
+namespace Nephrite.Metamodel
+{
+    public static class HistoryType
+    {
+        public const string None = "N";
+        public static string NoneTitle = "Нет";
+
+        public const string Object = "O";
+        public static string ObjectTitle = "Версионность объектов";
+
+        public const string IdentifiersRetain = "R";
+        public static string IdentifiersRetainTitle = "Версионность справочника с сохранением идентификаторов";
+
+        public const string IdentifiersMiss = "M";
+        public static string IdentifiersMissTitle = "Версионность справочника без сохранения идентификаторов";
+
+        public static List<CodifierValue> ToList()
+        {
+            if (_list == null)
+            {
+                _list = new List<CodifierValue>{
+                    new CodifierValue { Code = None, Title = NoneTitle},
+                    new CodifierValue { Code = Object, Title = ObjectTitle},
+                    new CodifierValue { Code = IdentifiersRetain, Title = IdentifiersRetainTitle},
+                    new CodifierValue { Code = IdentifiersMiss, Title = IdentifiersMissTitle},
+                };
+            }
+            return _list;
+        }
+        static List<CodifierValue> _list = null;
+        public static string Title(string code)
+        {
+            CodifierValue cv = ToList().FirstOrDefault(o => o.Code == code);
+            return cv != null ? cv.Title : String.Empty;
+        }
+    }
+}
