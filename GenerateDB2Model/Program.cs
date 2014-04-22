dc2b447b1a0cf51bf3b26e2980bf06a06fd93119
Aleksey <aleksey.tegay@live.ru>
2014-04-23 00:27:59 +0500

    Подправил генерацию COMMENT

--- GenerateDB2Model/Program.cs
+++ GenerateDB2Model/Program.cs
@@ -23,59 +23,59 @@ namespace GenerateDB2Model
 			var d2schema = db2.ReadSchema("DBO");
 
 			var sql = new SqlServerMetadataReader();
-			ConnectionManager.SetConnectionString("Password=q121212;Persist Security Info=True;User ID=servantsuser;Initial Catalog=servants;Data Source=srvsql.refactorx.ru\\mssqlserver2008");
+            ConnectionManager.SetConnectionString("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Servants20140421;Data Source=(local)");
 			//Base.Model = new DataContext(ConnectionManager.ConnectionString);
 			var sqlschema = sql.ReadSchema("DBO");
 
 
-			foreach (var dbTable in d2schema.Tables.Values)
+            foreach (var sT in sqlschema.Tables)
 			{
-				if (string.IsNullOrEmpty(dbTable.Description))
-				{
-					stringB.AppendFormat("COMMENT ON TABLE DBO.{0} IS '{1}'; \r\n", dbTable.Name, "|" + dbTable.Name);
-				}
-				else if (!dbTable.Description.Contains("|"))
-				{
-					// Проверим опсание на каком языке  ХАК
+                ////if (string.IsNullOrEmpty(dbTable.Description))
+                ////{
+                ////    stringB.AppendFormat("COMMENT ON TABLE DBO.{0} IS '{1}'; \r\n", dbTable.Name, "|" + dbTable.Name);
+                ////}
+                ////else if (!dbTable.Description.Contains("|"))
+                ////{
+                ////    // Проверим опсание на каком языке  ХАК
 
-					Regex regexTable = new Regex("[а-яА-ЯёЁъЪ]{1,32}");
-					Match matchTable = regexTable.Match(dbTable.Description);
-
-
-					stringB.AppendFormat("COMMENT ON TABLE DBO.{0} IS '{1}'; \r\n", dbTable.Name,
-					                     matchTable.Success ? (dbTable.Description + "|") : "|" + dbTable.Description );
-				}
-
-				foreach (var dbColumn in dbTable.Columns.Values)
-				{
-					if (string.IsNullOrEmpty(dbColumn.Description))
-					{
-						stringB.AppendFormat("COMMENT ON COLUMN DBO.{0}.{1} IS '{2}'; \r\n", dbTable.Name, dbColumn.Name, "|" + dbColumn.Name);
-					}
-					else if (!dbColumn.Description.Contains("|"))
-					{
-						// Проверим опсание на каком языке  ХАК
-
-						Regex regexTable = new Regex("[а-яА-ЯёЁъЪ]{1,32}");
-						Match matchTable = regexTable.Match(dbColumn.Description);
+                ////    Regex regexTable = new Regex("[а-яА-ЯёЁъЪ]{1,32}");
+                ////    Match matchTable = regexTable.Match(dbTable.Description);
 
 
-						stringB.AppendFormat("COMMENT ON COLUMN DBO.{0}.{1} IS '{2}'; \r\n", dbTable.Name, dbColumn.Name, matchTable.Success ? (dbColumn.Description + "|") : "|" + dbColumn.Description);
-					}
-				}
-				//if (d2schema.Tables.Values.Any(t => t.Name.ToLower() == sT.Value.Name.ToLower()))
-				//{
-				//	var dbTable = d2schema.Tables.Values.SingleOrDefault(t => t.Name.ToLower() == sT.Value.Name.ToLower());
-				//	if (dbTable != null)
-				//		stringB.AppendFormat("COMMENT ON TABLE DBO.{0} IS '{1}'; \r\n", dbTable.Name, string.IsNullOrEmpty(dbTable.Description) ? sT.Value.Name : dbTable.Description + "|" + sT.Value.Name);
-				//	foreach (var sColumn in sT.Value.Columns.Values)
-				//	{
-				//		var dbColumn = dbTable.Columns.Values.SingleOrDefault(t => t.Name.ToLower() == sColumn.Name.ToLower());
+                ////    stringB.AppendFormat("COMMENT ON TABLE DBO.{0} IS '{1}'; \r\n", dbTable.Name,
+                ////                         matchTable.Success ? (dbTable.Description + "|") : "|" + dbTable.Description );
+                ////}
 
-				//		if (dbColumn != null)
-				//			stringB.AppendFormat("COMMENT ON COLUMN DBO.{0}.{1} IS '{2}'; \r\n", dbTable.Name, dbColumn.Name, string.IsNullOrEmpty(dbColumn.Description) ? sColumn.Name : dbColumn.Description + "|" + sColumn.Name);
-				//	}
-				//}
+                ////foreach (var dbColumn in dbTable.Columns.Values)
+                ////{
+                ////    if (string.IsNullOrEmpty(dbColumn.Description))
+                ////    {
+                ////        stringB.AppendFormat("COMMENT ON COLUMN DBO.{0}.{1} IS '{2}'; \r\n", dbTable.Name, dbColumn.Name, "|" + dbColumn.Name);
+                ////    }
+                ////    else if (!dbColumn.Description.Contains("|"))
+                ////    {
+                ////        // Проверим опсание на каком языке  ХАК
+
+                ////        Regex regexTable = new Regex("[а-яА-ЯёЁъЪ]{1,32}");
+                ////        Match matchTable = regexTable.Match(dbColumn.Description);
+
+
+                ////        stringB.AppendFormat("COMMENT ON COLUMN DBO.{0}.{1} IS '{2}'; \r\n", dbTable.Name, dbColumn.Name, matchTable.Success ? (dbColumn.Description + "|") : "|" + dbColumn.Description);
+                ////    }
+                ////}
+                if (d2schema.Tables.Values.Any(t => t.Name.ToLower() == sT.Value.Name.ToLower()))
+                {
+                    var dbTable = d2schema.Tables.Values.SingleOrDefault(t => t.Name.ToLower() == sT.Value.Name.ToLower());
+                    if (dbTable != null)
+                        stringB.AppendFormat("COMMENT ON TABLE DBO.{0} IS '{1}'; \r\n", dbTable.Name, string.IsNullOrEmpty(dbTable.Description) ?"|"+ sT.Value.Name : dbTable.Description + "|" + sT.Value.Name);
+                    foreach (var sColumn in sT.Value.Columns.Values)
+                    {
+                        var dbColumn = dbTable.Columns.Values.SingleOrDefault(t => t.Name.ToLower() == sColumn.Name.ToLower());
+
+                        if (dbColumn != null)
+                            stringB.AppendFormat("COMMENT ON COLUMN DBO.{0}.{1} IS '{2}'; \r\n", dbTable.Name, dbColumn.Name, string.IsNullOrEmpty(dbColumn.Description) ? "|" + sColumn.Name : dbColumn.Description + "|" + sColumn.Name);
+                    }
+                }
 			}
 
 
