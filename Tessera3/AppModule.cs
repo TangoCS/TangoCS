c64ab005002a948c0852811daca3cd32e5e4178d
Vadim Lavrentyev <lubein@gmail.com>
2014-08-18 17:04:34 +0400

    Прототип нового сайта на Tessera3

---
+++ Tessera3/AppModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+
+namespace Tessera3Sample
+{
+	public class AppModule : IHttpModule
+	{
+		public void Dispose()
+		{
+		}
+
+		public void Init(HttpApplication context)
+		{
+			context.PostAuthenticateRequest += (o, e) =>
+			{
+				var r = context.Response;
+				r.Clear();
+
+				HtmlWriter h = new HtmlWriter();
+				h.DocType();
+				h.Html(() =>
+				{
+					h.Head();
+					h.Body(() => h.Write("Test"));
+				});
+
+				r.Write(h.ToString());
+				r.End();
+			};
+		}
+	}
+}
---
+++ Tessera3/HtmlWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Tessera3Sample
+{
+	public class HtmlWriter : StringWriter
+	{
+		public void DocType()
+		{
+			WriteLine("<!DOCTYPE html>");
+		}
+
+		public void Html(Action inner = null)
+		{
+			WriteLine("<html>");
+			if (inner != null) inner();
+			WriteLine("</html>");
+		}
+
+		public void Head(Action inner = null)
+		{
+			WriteLine("<head>");
+			if (inner != null) inner();
+			WriteLine("</head>");
+		}
+
+		public void Body(Action inner = null)
+		{
+			WriteLine("<body>");
+			if (inner != null) inner();
+			WriteLine("</body>");
+		}
+	}
+}
