2f1356f8b2546f72ff42f7230c3f3a42e22e2724
Vadim Lavrentyev <lubein@gmail.com>
2014-09-16 16:09:38 +0400

    postgres

---
+++ Nephrite.Meta.Database.PostgreSQL/PostgreSQLMetadataReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nephrite.Meta.Database
+{
+	public class PostgreSQLMetadataReader : IDatabaseMetadataReader
+    {
+		public Schema ReadSchema(string name)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<ProcedureDetails> ReadProceduresDetails()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
