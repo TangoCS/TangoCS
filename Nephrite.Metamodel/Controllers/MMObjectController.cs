99eb7809639af1d49ba1e543122b254f1c4c82cf
Берко Игорь <id.berko@gmail.com>
2014-09-17 15:46:47 +0300

    fix RenderMMView

--- Nephrite.Metamodel/Controllers/MMObjectController.cs
+++ Nephrite.Metamodel/Controllers/MMObjectController.cs
@@ -66,19 +66,24 @@ namespace Nephrite.Metamodel.Controllers
 				container.Controls.Add(lc);
                 if (line > 0)
                 {
-                    string text2 = "";
-
-					string[] lines = (new StreamReader(VirtualPathProvider.OpenFile(path))).ReadToEnd().Split(new string[] { "\r\n" }, StringSplitOptions.None);
-                    for (int i = 0; i < lines.Length; i++)
-                    {
-                        if (i + 1 == line)
-                            text2 += "<span style=\"color:Red; font-size:13px; font-weight:bold\">" + HttpUtility.HtmlEncode(lines[i]).Replace(" ", "&nbsp;").Replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;") + "</span";
-                        else
-                            text2 += HttpUtility.HtmlEncode(lines[i]).Replace(" ", "&nbsp;").Replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;");
-                        text2 += "<br />";
-                    }
-                    LiteralControl lc2 = new LiteralControl("<br /><br />" + text2);
-					container.Controls.Add(lc2);
+					using (Stream s = VirtualPathProvider.OpenFile(path))
+					{
+						using (StreamReader sr = new StreamReader(s))
+						{
+							string text2 = "";
+							string[] lines = sr.ReadToEnd().Split(new string[] { "\r\n" }, StringSplitOptions.None);
+							for (int i = 0; i < lines.Length; i++)
+							{
+								if (i + 1 == line)
+									text2 += "<span style=\"color:Red; font-size:13px; font-weight:bold\">" + HttpUtility.HtmlEncode(lines[i]).Replace(" ", "&nbsp;").Replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;") + "</span";
+								else
+									text2 += HttpUtility.HtmlEncode(lines[i]).Replace(" ", "&nbsp;").Replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;");
+								text2 += "<br />";
+							}
+							LiteralControl lc2 = new LiteralControl("<br /><br />" + text2);
+							container.Controls.Add(lc2);
+						}
+					}
                 }
             }
 			if (!container.Page.IsPostBack)
--- Nephrite.Metamodel/Controllers/PackageFormViewController.cs
+++ Nephrite.Metamodel/Controllers/PackageFormViewController.cs
@@ -54,19 +54,24 @@ namespace Nephrite.Metamodel.Controllers
                 WebPart.Controls.Add(lc);
                 if (line > 0)
                 {
-                    string text2 = "";
-
-					string[] lines = (new StreamReader(VirtualPathProvider.OpenFile(path))).ReadToEnd().Split(new string[] { "\r\n" }, StringSplitOptions.None);
-                    for (int i = 0; i < lines.Length; i++)
-                    {
-                        if (i + 1 == line)
-                            text2 += "<span style=\"color:Red; font-size:13px; font-weight:bold\">" + HttpUtility.HtmlEncode(lines[i]).Replace(" ", "&nbsp;").Replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;") + "</span";
-                        else
-                            text2 += HttpUtility.HtmlEncode(lines[i]).Replace(" ", "&nbsp;").Replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;");
-                        text2 += "<br />";
-                    }
-                    LiteralControl lc2 = new LiteralControl("<br /><br />" + text2);
-                    WebPart.Controls.Add(lc2);
+					using (Stream s = VirtualPathProvider.OpenFile(path))
+					{
+						using (StreamReader sr = new StreamReader(s))
+						{
+							string text2 = "";
+							string[] lines = sr.ReadToEnd().Split(new string[] { "\r\n" }, StringSplitOptions.None);
+							for (int i = 0; i < lines.Length; i++)
+							{
+								if (i + 1 == line)
+									text2 += "<span style=\"color:Red; font-size:13px; font-weight:bold\">" + HttpUtility.HtmlEncode(lines[i]).Replace(" ", "&nbsp;").Replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;") + "</span";
+								else
+									text2 += HttpUtility.HtmlEncode(lines[i]).Replace(" ", "&nbsp;").Replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;");
+								text2 += "<br />";
+							}
+							LiteralControl lc2 = new LiteralControl("<br /><br />" + text2);
+							WebPart.Controls.Add(lc2);
+						}
+					}
                 }
             }
         }
