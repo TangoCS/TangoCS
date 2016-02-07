0b73abb685864eac9d41661c92e92a63ff43eb7f
Vadim Lavrentyev <lubein@gmail.com>
2016-02-07 15:38:48 +0300

    Возврат на .net 4.5.1

--- Nephrite/Html/HtmlWriter.Input.cs
+++ Nephrite/Html/HtmlWriter.Input.cs
@@ -8,100 +8,70 @@ namespace Nephrite.Html
 	{
 		public static void Input(this IHtmlWriter w, string name, Action<InputTagAttributes> attributes = null)
 		{
-			Action<InputTagAttributes> a = ta => {
-				ta.Name(name).ID(name);
-				if (attributes != null) attributes(ta);
-			};
+			Action<InputTagAttributes> a = ta => ta.Name(name).ID(name).Set(attributes);
 			w.WriteTag("input", a, null, true);
 		}
 		public static void TextBox(this IHtmlWriter w, string name, string value = null, Action<InputTagAttributes> attributes = null)
 		{
-			Action<InputTagAttributes> a = ta => {
-				ta.Name(name).ID(name).Type(InputType.Text).Value(value);
-				if (attributes != null) attributes(ta);
-			};
+			Action<InputTagAttributes> a = ta => ta.Name(name).ID(name).Type(InputType.Text).Value(value).Set(attributes);
 			w.WriteTag("input", a, null, true);
 		}
 		public static void Password(this IHtmlWriter w, string name, string value = null, Action<InputTagAttributes> attributes = null)
 		{
-			Action<InputTagAttributes> a = ta => {
-				ta.Name(name).ID(name).Type(InputType.Password).Value(value);
-				if (attributes != null) attributes(ta);
-			};
+			Action<InputTagAttributes> a = ta => ta.Name(name).ID(name).Type(InputType.Password).Value(value).Set(attributes);
 			w.WriteTag("input", a, null, true);
 		}
 		public static void Hidden(this IHtmlWriter w, string name, string value = null, Action<InputTagAttributes> attributes = null)
 		{
-			Action<InputTagAttributes> a = ta => {
-				ta.Name(name).ID(name).Type(InputType.Hidden).Value(value);
-				if (attributes != null) attributes(ta);
-			};
+			Action<InputTagAttributes> a = ta => ta.Name(name).ID(name).Type(InputType.Hidden).Value(value).Set(attributes);
 			w.WriteTag("input", a, null, true);
 		}
 		public static void CheckBox(this IHtmlWriter w, string name, bool isChecked = false, Action<InputTagAttributes> attributes = null)
 		{
-			Action<InputTagAttributes> a = ta => {
-				ta.Name(name).ID(name).Type(InputType.Checkbox).Checked(isChecked);
-				if (attributes != null) attributes(ta);
-			};
+			Action<InputTagAttributes> a = ta => ta.Name(name).ID(name).Type(InputType.Checkbox).Checked(isChecked).Set(attributes);
 			w.WriteTag("input", a, null, true);
 		}
 		public static void RadioButton(this IHtmlWriter w, string name, bool isChecked = false, Action<InputTagAttributes> attributes = null)
 		{
-			Action<InputTagAttributes> a = ta => {
-				ta.Name(name).ID(name).Type(InputType.Radio).Checked(isChecked);
-				if (attributes != null) attributes(ta);
-			};
+			Action<InputTagAttributes> a = ta => ta.Name(name).ID(name).Type(InputType.Radio).Checked(isChecked).Set(attributes);
 			w.WriteTag("input", a, null, true);
 		}
 		public static void TextArea(this IHtmlWriter w, string name, string value = null, Action<TextAreaTagAttributes> attributes = null)
 		{
-			Action<TextAreaTagAttributes> a = ta => {
-				ta.Name(name).ID(name);
-				if (attributes != null) attributes(ta);
-			};
+			Action<TextAreaTagAttributes> a = ta => ta.Name(name).ID(name).Set(attributes);
 			w.WriteTag("textarea", a, () => w.Write(value));
 		}
 
 		public static void Button(this IHtmlWriter w, Action<ButtonTagAttributes> attributes, Action inner)
 		{
-			Action<ButtonTagAttributes> a = ta => {
-				ta.Type(ButtonType.Button);
-				if (attributes != null) attributes(ta);
-			};
+			Action<ButtonTagAttributes> a = ta => ta.Type(ButtonType.Button).Set(attributes);
 			w.WriteTag("button", a, inner);
 		}
 
 		public static void SubmitButton(this IHtmlWriter w, Action<ButtonTagAttributes> attributes, Action inner)
 		{
-			Action<ButtonTagAttributes> a = ta => {
-				ta.Type(ButtonType.Submit);
-				if (attributes != null) attributes(ta);
-			};
+			Action<ButtonTagAttributes> a = ta => ta.Type(ButtonType.Submit).Set(attributes);
 			w.WriteTag("button", a, inner);
 		}
 		public static void ResetButton(this IHtmlWriter w, Action<ButtonTagAttributes> attributes, Action inner)
 		{
-			Action<ButtonTagAttributes> a = ta => {
-				ta.Type(ButtonType.Reset);
-				if (attributes != null) attributes(ta);
-			};
+			Action<ButtonTagAttributes> a = ta => ta.Type(ButtonType.Reset).Set(attributes);
 			w.WriteTag("button", a, inner);
 		}
 
 		public static void Button(this IHtmlWriter w, Action<ButtonTagAttributes> attributes = null, string text = "OK")
 		{
-			w.Button(attributes, () => w.Write(text));
+			w.Button(a => a.Class("btn").Set(attributes), () => w.Write(text));
 		}
 
 		public static void SubmitButton(this IHtmlWriter w, Action<ButtonTagAttributes> attributes = null, string text = "OK")
 		{
-			w.SubmitButton(attributes, () => w.Write(text));
+			w.SubmitButton(a => a.Class("btn").Set(attributes), () => w.Write(text));
 		}
 
 		public static void ResetButton(this IHtmlWriter w, Action<ButtonTagAttributes> attributes = null, string text = "Reset")
 		{
-			w.ResetButton(attributes, () => w.Write(text));
+			w.ResetButton(a => a.Class("btn").Set(attributes), () => w.Write(text));
 		}
 
 	}
