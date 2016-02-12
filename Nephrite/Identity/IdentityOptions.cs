c768191064de109d56dcc1b5f0723f520151fc04
Vadim Lavrentyev <lubein@gmail.com>
2016-02-12 15:04:38 +0300

    git fix

---
+++ Nephrite/Identity/IdentityOptions.cs
@@ -0,0 +1,32 @@
+﻿namespace Nephrite.Identity
+{
+	public interface IIdentityOptions
+	{
+		bool AllowPasswordReset { get; }
+		bool AllowRegister { get; }
+		bool AllowRememberMe { get; }
+		string AnonymousUserName { get; }
+		bool Enabled { get; }
+		string SystemUserName { get; }
+	}
+
+	public class IdentityOptions : IIdentityOptions
+	{
+		public string AnonymousUserName { get; set; }
+		public string SystemUserName { get; set; }
+		public bool Enabled { get; set; }
+		public bool AllowRegister { get; set; }
+		public bool AllowPasswordReset { get; set; }
+		public bool AllowRememberMe { get; set; }
+
+		public IdentityOptions()
+		{
+			AnonymousUserName = "anonymous";
+			SystemUserName = "system";
+			Enabled = true;
+			AllowRegister = false;
+			AllowPasswordReset = false;
+			AllowRememberMe = true;
+		}
+	}
+}
