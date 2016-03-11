29a68317197d2f3802a2f848794220a98e6d07e9
Берко Игорь <id.berko@gmail.com>
2016-03-11 15:09:13 +0300

    fix azurebackup

--- Nephrite.AzureBackup/AzureBackup.cs
+++ Nephrite.AzureBackup/AzureBackup.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure;
@@ -147,5 +150,41 @@ namespace Nephrite.AzureBackup
 				}
 			}
 		}
+	}
+
+	public class Impersonation : IDisposable
+	{
+		private IntPtr userToken = IntPtr.Zero;
+		private WindowsImpersonationContext impersonatedUser = null;
+		private const int LOGON32_PROVIDER_DEFAULT = 0;
+		private const int LOGON32_LOGON_INTERACTIVE = 2;
+
+		public Impersonation(string domain, string user, string password)
+		{
+			if (user == null || user.Length == 0)
+				return;
+			if (!LogonUser(user, domain, password,
+			LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT,
+			ref userToken))
+				throw new SecurityException();
+			WindowsIdentity newId = new WindowsIdentity(userToken);
+			impersonatedUser = newId.Impersonate();
+		}
+
+		public void Dispose()
+		{
+			if (impersonatedUser != null)
+			{
+				impersonatedUser.Undo();
+				CloseHandle(userToken);
+			}
+		}
+
+		[DllImport("advapi32.dll", SetLastError = true)]
+		private static extern bool LogonUser(String lpszUsername, String lpszDomain, String lpszPassword,
+		int dwLogonType, int dwLogonProvider, ref IntPtr phToken);
+
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+		private extern static bool CloseHandle(IntPtr handle);
 	}
 }
--- Nephrite.AzureBackup/BackupMSSQL.cs
+++ Nephrite.AzureBackup/BackupMSSQL.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ICSharpCode.SharpZipLib.Zip;
-using Nephrite.Web;
 
 namespace Nephrite.AzureBackup
 {
