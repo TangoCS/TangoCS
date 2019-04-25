7413318b3923002da57e5ad4d3d2daac68b68008
Vadim Lavrentyev <lubein@gmail.com>
2019-04-25 10:29:19 +0300

    Добавление атрибутов в календарь

--- Tango/UI/Calendar.cs
+++ Tango/UI/Calendar.cs
@@ -3,22 +3,31 @@ using Tango.Html;
 
 namespace Tango.UI
 {
+	public class CalendarOptions
+	{
+		public bool Enabled { get; set; } = true;
+		public bool ShowTime { get; set; } = false;
+		public bool UseCalendarDays { get; set; } = false;
+		public Action<InputTagAttributes> Attributes { get; set; }
+	}
+
 	public static class CalendarExtension
 	{
-		public static void Calendar(this LayoutWriter w, InputName name, DateTime? value = null, bool enabled = true, bool showTime = false, bool useCalendarDaysInJSCalendar = false)
+		public static void Calendar(this LayoutWriter w, InputName name, DateTime? value, CalendarOptions options)
 		{
+			if (options == null) options = new CalendarOptions();
 			string basePath = GlobalSettings.JSPath + "calendar/";
 
 			//c.Page.RegisterScript("calendar-setup", basePath + "calendar-setup_stripped.js");
 			if (value == DateTime.MinValue) value = null;
 
-			w.TextBox(name, showTime ? value.DateTimeToString() : value.DateToString(), a =>
-				a.Data("format", "dd.MM.yyyy").Placeholder("ДД.ММ.ГГГГ").Style("width:" + (showTime ? "130px" : "100px"))
+			w.TextBox(name, options.ShowTime ? value.DateTimeToString() : value.DateToString(), a =>
+				a.Data("format", "dd.MM.yyyy").Placeholder("ДД.ММ.ГГГГ").Style("width:" + (options.ShowTime ? "130px" : "100px"))
 				.Data("calendar", "")
-				.Data("showtime", showTime).Data("usecalendardays", useCalendarDaysInJSCalendar)
-				.OnKeyPress("return calendarcontrol.keypress(event)").Disabled(!enabled)
+				.Data("showtime", options.ShowTime).Data("usecalendardays", options.UseCalendarDays)
+				.OnKeyPress("return calendarcontrol.keypress(event)").Disabled(!options.Enabled).Set(options.Attributes)
 			);
-			if (enabled)
+			if (options.Enabled)
 			{
 				w.Img(a => a.ID("btn" + name.ID).Title("Календарь").Src(basePath + "img.gif"));
 
@@ -31,34 +40,38 @@ namespace Tango.UI
 					button = f("btn" + name.ID),
 					showOthers = true,
 					weekNumbers = false,
-					showTime = showTime,
-					ifFormat = showTime ? "%d.%m.%Y %H:%M" : "%d.%m.%Y",
+					showTime = options.ShowTime,
+					ifFormat = options.ShowTime ? "%d.%m.%Y %H:%M" : "%d.%m.%Y",
 					timeFormat = "24",
-					dateStatusFunc = useCalendarDaysInJSCalendar ? "jscal_calendarDate" : null
+					dateStatusFunc = options.UseCalendarDays ? "jscal_calendarDate" : null
 				});
 			}
 
-//				if (ConfigurationManager.AppSettings["UseCalendarDaysInJSCalendar"] == "true")
-//				{
-//					c.Page.RegisterStartupScript("calendar-workdays", @"
-//function jscal_calendarDate_c(arr, date) {
-//	for(var i = 0; i < arr.length; i++)
-//		if( arr[i].toString() == date.toString())
-//			return true;
-//	return false;
-//}
-//function jscal_calendarDate(date, y, m, d){" + CalendarDayCache.ToJSArray("w", "h") + @"
-//	if (jscal_calendarDate_c(w, new Date(date.getFullYear(), date.getMonth(), d)))
-//		return 'cal-workingday';
-//	if (jscal_calendarDate_c(h, new Date(date.getFullYear(), date.getMonth(), d)))
-//		return 'cal-holiday';
-//	if (date.getDay() == 0 || date.getDay() == 6)
-//		return 'cal-holiday';
-//	else
-//		return 'cal-workingday';
-//}");
-//				}
-			
+			//				if (ConfigurationManager.AppSettings["UseCalendarDaysInJSCalendar"] == "true")
+			//				{
+			//					c.Page.RegisterStartupScript("calendar-workdays", @"
+			//function jscal_calendarDate_c(arr, date) {
+			//	for(var i = 0; i < arr.length; i++)
+			//		if( arr[i].toString() == date.toString())
+			//			return true;
+			//	return false;
+			//}
+			//function jscal_calendarDate(date, y, m, d){" + CalendarDayCache.ToJSArray("w", "h") + @"
+			//	if (jscal_calendarDate_c(w, new Date(date.getFullYear(), date.getMonth(), d)))
+			//		return 'cal-workingday';
+			//	if (jscal_calendarDate_c(h, new Date(date.getFullYear(), date.getMonth(), d)))
+			//		return 'cal-holiday';
+			//	if (date.getDay() == 0 || date.getDay() == 6)
+			//		return 'cal-holiday';
+			//	else
+			//		return 'cal-workingday';
+			//}");
+			//				}
+		}
+
+		public static void Calendar(this LayoutWriter w, InputName name, DateTime? value = null, bool enabled = true, bool showTime = false, bool useCalendarDaysInJSCalendar = false)
+		{
+			w.Calendar(name, value, new CalendarOptions { Enabled = enabled, ShowTime = showTime, UseCalendarDays = useCalendarDaysInJSCalendar });
 		}
 	}
 
