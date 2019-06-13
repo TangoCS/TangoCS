3ea47a9de11776b2ace77b31f1bec0fa95c6c38d
nephritetechmaks <makstl@yandex.ru>
2019-06-13 16:02:39 +0300

    Перенос интерфейсов по журналу операций

---
+++ Tango/Data/IEntityAudit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tango.Data
+{
+	public interface IEntityAudit
+	{
+		void WriteObjectChange<T, TKey>(T entity, EntityAuditAction action, List<PropertyChange> propertyChanges) where T : IWithKey<T, TKey>;
+	}
+
+	public class PropertyChange
+	{
+		public string PropertyName { get; set; }
+		public string OldValue { get; set; }
+		public string NewValue { get; set; }
+	}
+
+	public enum EntityAuditAction
+	{
+		Insert,
+		Update,
+		Delete
+	}
+
+	public interface IObjectTracker
+	{
+		void StartTracking(object entity);
+		List<PropertyChange> GetChanges(object entity);
+	}
+}
