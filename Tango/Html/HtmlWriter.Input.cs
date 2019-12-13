d80915a6f40a611cc7453c4843ea69bbdd4860ae
Vadim Lavrentyev <lubein@gmail.com>
2019-12-13 22:27:14 +0300

    fix рендеринга inputs

--- Tango/Html/HtmlWriter.Input.cs
+++ Tango/Html/HtmlWriter.Input.cs
@@ -7,34 +7,52 @@ namespace Tango.Html
 	{
 		public static void Input(this HtmlWriter w, InputName name, Action<InputTagAttributes> attributes = null)
 		{
-			Action<InputTagAttributes> a = ta => ta.Name(name.Name).ID(name.ID).Set(attributes);
+			Action<InputTagAttributes> a = ta => {
+				if (name != null) ta.Name(name.Name).ID(name.ID);
+				ta.Set(attributes);
+			};
 			w.WriteSelfClosingTag("input", a);
 		}
 		public static void TextBox(this HtmlWriter w, InputName name, string value = null, Action<InputTagAttributes> attributes = null)
 		{
 			value = WebUtility.HtmlEncode(value);
-			Action<InputTagAttributes> a = ta => ta.Name(name.Name).ID(name.ID).Type(InputType.Text).Value(value).Set(attributes);
+			Action<InputTagAttributes> a = ta => {
+				if (name != null) ta.Name(name.Name).ID(name.ID);
+				ta.Type(InputType.Text).Value(value).Set(attributes);
+			};
 			w.WriteSelfClosingTag("input", a);
 		}
 		public static void Password(this HtmlWriter w, InputName name, string value = null, Action<InputTagAttributes> attributes = null)
 		{
-			Action<InputTagAttributes> a = ta => ta.Name(name.Name).ID(name.ID).Type(InputType.Password).Value(value).Set(attributes);
+			Action<InputTagAttributes> a = ta => {
+				if (name != null) ta.Name(name.Name).ID(name.ID);
+				ta.Type(InputType.Password).Value(value).Set(attributes);
+			};
 			w.WriteSelfClosingTag("input", a);
 		}
 		public static void Hidden(this HtmlWriter w, InputName name, string value = null, Action<InputTagAttributes> attributes = null)
 		{
 			value = WebUtility.HtmlEncode(value);
-			Action<InputTagAttributes> a = ta => ta.Name(name.Name).ID(name.ID).Type(InputType.Hidden).Value(value).Set(attributes);
+			Action<InputTagAttributes> a = ta => {
+				if (name != null) ta.Name(name.Name).ID(name.ID);
+				ta.Type(InputType.Hidden).Value(value).Set(attributes);
+			};
 			w.WriteSelfClosingTag("input", a);
 		}
 		public static void CheckBox(this HtmlWriter w, InputName name, bool isChecked = false, Action<InputTagAttributes> attributes = null)
 		{
-			Action<InputTagAttributes> a = ta => ta.Name(name.Name).ID(name.ID).Type(InputType.Checkbox).Checked(isChecked).Set(attributes);
+			Action<InputTagAttributes> a = ta => {
+				if (name != null) ta.Name(name.Name).ID(name.ID);
+				ta.Type(InputType.Checkbox).Checked(isChecked).Set(attributes);
+			};
 			w.WriteSelfClosingTag("input", a);
 		}
 		public static void CheckBox(this HtmlWriter w, InputName name, string value, bool isChecked = false, Action<InputTagAttributes> attributes = null)
 		{
-			Action<InputTagAttributes> a = ta => ta.Name(name.Name).ID(name.ID).Type(InputType.Checkbox).Value(value).Checked(isChecked).Set(attributes);
+			Action<InputTagAttributes> a = ta => {
+				if (name != null) ta.Name(name.Name).ID(name.ID);
+				ta.Type(InputType.Checkbox).Value(value).Checked(isChecked).Set(attributes);
+			};
 			w.WriteSelfClosingTag("input", a);
 		}
 		public static void RadioButton(this HtmlWriter w, string name, string id, string value = null, bool isChecked = false, Action<InputTagAttributes> attributes = null)
@@ -45,7 +63,10 @@ namespace Tango.Html
 		public static void TextArea(this HtmlWriter w, InputName name, string value = null, Action<TextAreaTagAttributes> attributes = null)
 		{
 			value = WebUtility.HtmlEncode(value);
-			Action<TextAreaTagAttributes> a = ta => ta.Name(name.Name).ID(name.ID).Set(attributes);
+			Action<TextAreaTagAttributes> a = ta => {
+				if (name != null) ta.Name(name.Name).ID(name.ID);
+				ta.Set(attributes);
+			};
 			w.WriteTag("textarea", a, () => w.Write(value));
 		}
 
