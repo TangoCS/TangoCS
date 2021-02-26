e0830f66b62197f94db5bc482d20c81cbd122457
Eugeny Belovolov <belovolov-eg@it-energy.ru>
2021-02-26 18:18:57 +0300

    добавлена ; в конец OnClick

--- Tango.UI.Std/UI/ActionLinkExtensions.cs
+++ Tango.UI.Std/UI/ActionLinkExtensions.cs
@@ -218,7 +218,7 @@ namespace Tango.UI
 		public static void CopyToClipboardButton(this LayoutWriter w, Action<ATagAttributes> attrs, string contentElementId, Action content)
 		{
 			var el = w.GetID(contentElementId);
-			w.A(a => a.Class("actionbtn").Set(attrs).OnClick($"commonUtils.copyToClipboard('{el}')"), content);
+			w.A(a => a.Class("actionbtn").Set(attrs).OnClick($"commonUtils.copyToClipboard('{el}');"), content);
 		}
 
 		public static void ExternalImageLink(this LayoutWriter w, string href)
