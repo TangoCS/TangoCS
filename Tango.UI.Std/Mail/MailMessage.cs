371c574d914e02cecf739ff7c24d16d75e23c0e9
Eugeny Belovolov <belovolov-eg@it-energy.ru>
2021-04-09 12:17:03 +0300

    Журнал почтовых отправок.

---
+++ Tango.UI.Std/Mail/MailMessage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq.Expressions;
+using Tango.Data;
+
+namespace Tango.Mail
+{
+    [BaseNamingConventions(Category = BaseNamingEntityCategory.Mail)]
+    public class MailMessage: IEntity, IWithKey<MailMessage, int>, IWithTitle
+    {
+        public string Title { get; set; }
+        
+        public virtual Expression<Func<MailMessage, bool>> KeySelector(int id)
+        {
+            return o => o.MailMessageID == id;
+        }
+        public virtual int ID => MailMessageID;
+        [Key]
+        [Identity]
+        [Column]
+        public virtual int MailMessageID { get; set; }
+        [Column]
+        public virtual string Recipients { get; set; }
+        [Column]
+        public virtual string Subject { get; set; }
+        [Column]
+        public virtual string Body { get; set; }
+        [Column]
+        public virtual int MailMessageStatusID { get; set; }
+        [Column]
+        public virtual string AttachmentName { get; set; }
+        [Column]
+        public virtual byte[] Attachment { get; set; }
+        [Column]
+        public virtual string Error { get; set; }
+        [Column]
+        public virtual string CopyRecipients { get; set; }
+        [Column]
+        public virtual int TimeoutValue { get; set; }
+        [Column]
+        public virtual DateTime? LastSendAttemptDate { get; set; }
+        [Column]
+        public virtual int AttemptsToSendCount { get; set; }
+        [Column]
+        public virtual DateTime LastModifiedDate { get; set; }
+        
+        public string MailMessageStatus { get; set; }
+    }
+}
---
+++ Tango.UI.Std/Mail/MailMessage_allviews.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq.Expressions;
+using Tango.UI;
+using Tango.UI.Controls;
+using Tango.UI.Std;
+
+namespace Tango.Mail
+{
+    [OnAction(typeof(MailMessage), "viewlist")]
+    public class MailMessage_viewlist : default_list_rep<MailMessage>
+    {
+        
+        protected override Func<string, Expression<Func<MailMessage, bool>>> SearchExpression => s => {
+            return o => o.Subject.Contains(s) || o.Recipients.Contains(s); 
+        };
+
+        protected override void ToolbarLeft(MenuBuilder t)
+        {
+            t.ItemFilter(Filter);
+        }
+		
+        protected override void FieldsInit(FieldCollection<MailMessage> f)
+        {
+            f.SetRowID(o => o.MailMessageID.ToString());
+
+            f.AddCellWithSortAndFilter(o => o.Recipients, o => o.Recipients);
+            f.AddCellWithSortAndFilter(o => o.Subject, o => o.Subject);
+            f.AddCellWithSortAndFilter(o => o.Body, o => o.Body);
+            f.AddCellWithSortAndFilter(o => o.MailMessageStatus, o => o.MailMessageStatus);
+            f.AddCellWithSortAndFilter(o => o.TimeoutValue, o => o.TimeoutValue);
+            f.AddCellWithSortAndFilter(o => o.AttachmentName, o => o.AttachmentName);
+            f.AddCellWithSortAndFilter(o => o.Error, o => o.Error);
+            f.AddCellWithSortAndFilter(o => o.CopyRecipients, o => o.CopyRecipients);
+            f.AddCellWithSortAndFilter(o => o.LastSendAttemptDate, o => o.LastSendAttemptDate.DateTimeToString());
+            f.AddCellWithSortAndFilter(o => o.AttemptsToSendCount, o => o.AttemptsToSendCount);
+            f.AddCellWithSortAndFilter(o => o.LastModifiedDate, o => o.LastModifiedDate.DateTimeToString());
+        }
+    }
+}
