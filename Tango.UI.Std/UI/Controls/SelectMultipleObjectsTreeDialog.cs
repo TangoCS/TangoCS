4fef2f86b9fd3c5486116e8889edc7984b6a1365
Vadim <Vadim@VADIM-HOME-3>
2021-05-12 10:33:30 +0300

    Рефакторинг окон выбора

---
+++ Tango.UI.Std/UI/Controls/SelectMultipleObjectsTreeDialog.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Tango.Html;
+using Tango.Localization;
+
+namespace Tango.UI.Controls
+{
+	public class SelectMultipleObjectsTreeDialog<TRef, TRefKey> : 
+		SelectObjectDialog<TRef, TRefKey, IEnumerable<TRef>, AbstractSelectMultipleObjectsField<TRef, TRefKey>>
+		where TRef : class, IWithTitle, IWithKey<TRefKey>
+		//where TField : AbstractSelectMultipleObjectsField<TRef, TRefKey>
+	{
+		public string OpenDialogLinkTitle { get; set; } = null;
+
+		public override void List(LayoutWriter w, IEnumerable<TRef> data)
+		{
+			w.Div(a => a.ID().DataCtrl("selectMultipleObjectsDialog"), () => {
+				w.Div(a => a.Class("checkboxlist"), () => {
+					foreach (var o in data)
+					{
+						var value = Field.DataValueField(o);
+						w.Label(a => a.For("item" + value), () => {
+							w.CheckBox("item", false, a => a.ID("item" + value).Value(value));
+							Field.DataRow(w, o);
+						});
+					}
+				});
+			});
+			AfterList?.Invoke(w);
+		}
+
+		public override void Render(LayoutWriter w, IEnumerable<TRef> selectedValues)
+		{
+			var cw = w.Clone(Field);
+			var pw = w.Clone(Field.ParentElement);
+			cw.Div(a => a.ID("placeholder"), () => {
+				pw.Hidden(Field.ID, selectedValues?.Select(o => Field.DataValueField(o)).Join(","),
+					a => a.DataHasClientState(ClientStateType.Array, ClientID, "selectedvalues"));
+				if (!Field.Disabled)
+				{
+					cw.A(a => {
+						a.Data(Field.DataCollection);
+						if (Field.DoCallbackToCurrent)
+							a.CallbackToCurrent(Context);
+
+						a.AsDialog(OpenDialog, Field.ClientID);
+					}, OpenDialogLinkTitle ?? Resources.Get("Common.SelectObjects_Field"));
+					cw.Write("&nbsp;");
+					if (!Field.PostOnClearEvent)
+						cw.A(a => a.OnClick($"selectMultipleObjectsDialog.clear('{Field.ClientID}', true)"),
+							Resources.Get("Common.Clear"));
+					else
+						cw.A(a => a.OnClickPostEvent(OnClear), Resources.Get("Common.Clear"));
+					if (Field.FieldExtensions != null)
+					{
+						cw.Write("&nbsp;");
+						Field.FieldExtensions(cw);
+					}
+				}
+				RenderSelected(cw, selectedValues);
+			});
+		}
+
+		public override void RenderSelected(LayoutWriter cw, IEnumerable<TRef> selectedValues)
+		{
+			cw.Div(a => a.ID("selected"), () => {
+				if (Field.Disabled && Field.TextWhenDisabled != null)
+					Field.TextWhenDisabled?.Invoke(cw);
+				else if (selectedValues != null && selectedValues.Count() > 0)
+					cw.WriteLines(selectedValues.Select(o => Field.SelectedObjectTextField(o)));
+				else
+					Field.TextWhenNothingSelected?.Invoke(cw);
+			});
+		}
+
+		public void OnClear(ApiResponse response)
+		{
+			response.AddWidget(Field.ID + "_selected", w => Field.TextWhenNothingSelected?.Invoke(w));
+			response.AddClientAction("selectMultipleObjectsDialog", "clear", Field.ClientID);
+			Field.OnClear(response);
+		}
+
+		public override void SubmitDialog(ApiResponse response)
+		{
+			var ids = Context.GetListArg<TRefKey>(Field.ID);
+			var selectedValues = Field.GetObjectsByIDs(ids);
+
+			response.WithNamesAndWritersFor(Field);
+			response.ReplaceWidget("placeholder", w => {
+				Render(w, selectedValues);
+			});
+			Field.OnChange(response, selectedValues);
+		}
+	}
+}
---
+++ Tango.UI.Std/UI/Controls/SelectObjectFieldDataProvider.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Tango.Data;
+
+namespace Tango.UI.Controls
+{
+	public interface ISelectObjectFieldDataProvider<TRef>
+	{
+		int GetCount();
+		IEnumerable<TRef> GetData(Paging paging);
+		IEnumerable<TRef> GetAllData();
+		TRef GetObjectByID<T>(T id, Expression<Func<TRef, bool>> predicate);
+		IEnumerable<TRef> GetObjectsByID<T>(IEnumerable<T> id, Expression<Func<TRef, bool>> predicate);
+	}
+
+	public class ORMSelectObjectFieldDataProvider<TRef> : ISelectObjectFieldDataProvider<TRef>
+	{
+		ISelectObjectField<TRef> field;
+
+		public ORMSelectObjectFieldDataProvider(ISelectObjectField<TRef> field)
+		{
+			this.field = field;
+		}
+
+		public IEnumerable<TRef> GetAllData()
+		{
+			return field.AllObjects;
+		}
+
+		public int GetCount()
+		{
+			return field.ItemsCountQuery().Count();
+		}
+
+		public IEnumerable<TRef> GetData(Paging paging)
+		{
+			return field.DataQuery(paging);
+		}
+
+		public TRef GetObjectByID<T>(T id, Expression<Func<TRef, bool>> predicate)
+		{
+			return field.AllObjects.Where(predicate).FirstOrDefault();
+		}
+
+		public IEnumerable<TRef> GetObjectsByID<T>(IEnumerable<T> id, Expression<Func<TRef, bool>> predicate)
+		{
+			return field.AllObjects.Where(predicate).ToList();
+		}
+	}
+
+	public class DapperSelectObjectFieldDataProvider<TRef> : ISelectObjectFieldDataProvider<TRef>
+	{
+		ISelectObjectField<TRef> field;
+		IDatabase database;
+
+		public string AllObjectsSql { get; set; }
+		public object AllObjectsSqlParms { get; set; }
+		public Func<IQueryable<TRef>, IQueryable<TRef>> PostProcessing { get; set; }
+
+		public DapperSelectObjectFieldDataProvider(ISelectObjectField<TRef> field, IDatabase database)
+		{
+			this.field = field;
+			this.database = database;
+			if (field.AllObjects == null) field.AllObjects = Enumerable.Empty<TRef>().AsQueryable();
+		}
+
+		public IEnumerable<TRef> GetAllData()
+		{
+			var rep = database.Repository<TRef>().WithAllObjectsQuery(AllObjectsSql, AllObjectsSqlParms);
+			var q = field.AllObjects;
+			if (PostProcessing != null) q = PostProcessing(q);
+			return rep.List(q.Expression);
+		}
+
+		public int GetCount()
+		{
+			var rep = database.Repository<TRef>().WithAllObjectsQuery(AllObjectsSql, AllObjectsSqlParms);
+			var q = field.ItemsCountQuery();
+			return rep.Count(q.Expression);
+		}
+
+		public IEnumerable<TRef> GetData(Paging paging)
+		{
+			var rep = database.Repository<TRef>().WithAllObjectsQuery(AllObjectsSql, AllObjectsSqlParms);
+			var q = field.DataQuery(paging);
+			if (PostProcessing != null) q = PostProcessing(q);
+			return rep.List(q.Expression);
+		}
+
+		public TRef GetObjectByID<T>(T id, Expression<Func<TRef, bool>> predicate)
+		{
+			var rep = database.Repository<TRef>().WithAllObjectsQuery(AllObjectsSql, AllObjectsSqlParms);
+			var q = field.AllObjects.Where(predicate);
+			return rep.List(q.Expression).FirstOrDefault();
+		}
+
+		public IEnumerable<TRef> GetObjectsByID<T>(IEnumerable<T> id, Expression<Func<TRef, bool>> predicate)
+		{
+			var rep = database.Repository<TRef>().WithAllObjectsQuery(AllObjectsSql, AllObjectsSqlParms);
+			var q = field.AllObjects.Where(predicate);
+			return rep.List(q.Expression);
+		}
+	}
+}
