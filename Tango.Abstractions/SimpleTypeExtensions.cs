1362019b87389344bbf9bf9928548f3189694917
ananiev-pi <ananiev.petr.ivanovich@gmail.com>
2021-05-18 17:45:27 +0300

    Вычисление дня недели

--- Tango.Abstractions/SimpleTypeExtensions.cs
+++ Tango.Abstractions/SimpleTypeExtensions.cs
@@ -279,6 +279,15 @@ namespace Tango
 		{
 			return date.AddMonths(1).AddDays(-1).EndDay();
 		}
+		public static DateTime GetLastWeekdayOfMonth(this DateTime date, DayOfWeek day)
+		{
+			DateTime lastDayOfMonth = new DateTime(date.Year, date.Month, 1)
+				.AddMonths(1).AddDays(-1);
+			int wantedDay = (int)day;
+			int lastDay = (int)lastDayOfMonth.DayOfWeek;
+			return lastDayOfMonth.AddDays(
+				lastDay >= wantedDay ? wantedDay - lastDay : wantedDay - lastDay - 7);
+		}
 		public static bool IsInRange(this DateTime dateToCheck, DateTime startDate, DateTime endDate)
 		{
 			return dateToCheck >= startDate && dateToCheck < endDate;
