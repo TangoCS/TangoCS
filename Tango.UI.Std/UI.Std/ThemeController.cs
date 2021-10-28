228a72024a5cbaee52c1abb5623b8897181b67b4
Vadim <Vadim@VADIM-HOME-3>
2021-10-28 12:02:58 +0300

    ThemeController

---
+++ Tango.UI.Std/UI.Std/ThemeController.cs
@@ -0,0 +1,14 @@
+﻿namespace Tango.UI.Std
+{
+	public class ThemeController : BaseController
+	{
+		[AllowAnonymous]
+		public ActionResult ChangeTheme(string newtheme)
+		{
+			return new ApiResult(response => {
+				response.AddClientAction("domActions", "setCookie", f => new { id = "theme", value = newtheme });
+				response.HardRedirectTo("/");
+			});
+		}
+	}
+}
