2366bfe8352a620f97fed2a3ec9a68938291b278
Берко Игорь <id.berko@gmail.com>
2022-06-30 11:37:32 +0300

    listview формирование иконки через HtmlWriter.Icon

--- Tango.Client.Js/wwwroot/js/tango/listview.js
+++ Tango.Client.Js/wwwroot/js/tango/listview.js
@@ -767,20 +767,8 @@ var listview = function (au, cu, cbcell, menu) {
 	}
 
 	function addDelIcon(el) {
-		var del = document.createElement("I");
-		del.className = 'icon icon-delete';
-
-		var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
-			use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
-
-		use.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '/data/icons/svg#icon-delete');
-
-		svg.className = 'svgicon-delete';
-		svg.appendChild(use);
-
-		del.appendChild(svg);
+		var del = new Tango.HtmlWriter().Icon('delete');
 		el.appendChild(del);
-
 		del.addEventListener('click', instance.onRemoveIconClick);
 	}
 
