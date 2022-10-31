ab5731ed6c27b17cdb16b6c305f83dc4ee6b74a0
Максим Федоров <maks.gm@gmail.com>
2022-10-31 14:58:11 +0300

    SelectListItem.Disabled

--- Tango/CodifierValue.cs
+++ Tango/CodifierValue.cs
@@ -16,6 +16,7 @@ namespace Tango
 		public string Text { get; set; }
 		public string Value { get; set; }
 		public bool Selected { get; set; }
+		public bool Disabled { get; set; }
 
 		public SelectListItem()
 		{
