de332c88c52eb09054af3f5d32b2b0fea1abe829
MAIN\Morozov_DA <morozov-da@it-energy.ru>
2022-11-23 18:27:39 +0300

    Fix Return chandge

--- Tango.UI.Std/UI/FieldsRendering.cs
+++ Tango.UI.Std/UI/FieldsRendering.cs
@@ -177,10 +177,10 @@ namespace Tango.UI
 			w.DropDownList(field.ID, value, items, attrs);
 		}
 
-		public static void DropDownList<TValue>(this LayoutWriter w, IField<TValue> field, IEnumerable<SelectListItem> items, GridPosition grid = null, Action<SelectTagAttributes> attrs = null, string hint = null, bool selectedOptionHints = false, Action caption=null)
+		public static void DropDownList<TValue>(this LayoutWriter w, IField<TValue> field, IEnumerable<SelectListItem> items, GridPosition grid = null, Action<SelectTagAttributes> attrs = null, string hint = null, bool selectedOptionHints = false)
 		{
 			var value = typeof(TValue).IsEnum ?
-				field.Value == null ? "" : Convert.ChangeType(field.Value, Enum.GetUnderlyingType(typeof(TValue))).ToString() : 
+				field.Value == null ? "" : Convert.ChangeType(field.Value, Enum.GetUnderlyingType(typeof(TValue))).ToString() :
 				field.Value?.ToString();
 
 			if (field.FireOnChangeEvent && !field.Disabled && field.IsVisible)
@@ -193,10 +193,7 @@ namespace Tango.UI
 				w.AddClientAction("domActions", "setAttribute", f => new { id = f(field.ID), attrName = "readonly", attrValue = "readonly" });
 			if (selectedOptionHints)
 				w.AddClientAction("commonUtils", "selectedOptionHints", f => new { id = f(field.ID) });
-			if(caption == null)
-				w.FormField(field, () => w.DropDownList(field.ID, value, items, attrs), grid);
-			else
-				w.FormField(field.Caption, caption, () => w.DropDownList(field.ID, value, items, attrs), grid);
+			w.FormField(field, () => w.DropDownList(field.ID, value, items, attrs), grid);
 		}
 
 		//public static void Calendar(this LayoutWriter w, IField<DateTime> field, Action<InputTagAttributes> attributes = null, GridPosition grid = null)
