89e8a288931305ff61f7f54dfd6f57eecd0d8945
Илья <odinokov-iv@it-energy.ru>
2023-03-02 15:10:55 +0300

    Validation Block 100 Percent

--- Tango.UI.Std/UI.Std/ValidateRequiredFormatter.cs
+++ Tango.UI.Std/UI.Std/ValidateRequiredFormatter.cs
@@ -7,50 +7,44 @@ namespace Tango.UI.Std
 {
     public static class ValidationFormatter
     {
-        // public static LayoutWriter ValidationBlock(this LayoutWriter w, ValidationMessageCollection val)
-        // {
-        //     w.Fieldset(a => a.Style("margin-bottom:8px"), () =>
-        //     {
-        //         w.Ul(a => a.Class("savewarning"), () =>
-        //         {
-        //             foreach (var item in val)
-        //                 w.Li(() => w.Write(item.Message));
-        //         });
-        //     });
-        //     return w;
-        // }
+        public static LayoutWriter ValidationBlock(this LayoutWriter w, ValidationMessageCollection val) => 
+            ValidationBlockImpl(w, val, a => a.Class("widthstd"));
 
-        public static void ValidationBlockContainer(this LayoutWriter w, ValidationMessageSeverity severity, Action content)
+        public static LayoutWriter ValidationBlock100Percent(this LayoutWriter w, ValidationMessageCollection val) =>
+            ValidationBlockImpl(w, val, a => a.Class("width100"));
+
+        public static void ValidationBlockContainer(this LayoutWriter w, ValidationMessageSeverity severity, Action content, Action<TagAttributes> attributes = null)
         {
-			var color = 
-				severity == ValidationMessageSeverity.Error ? "red" :
-				severity == ValidationMessageSeverity.Warning ? "yellow" :
-				"skyblue";
+            var color =
+                severity == ValidationMessageSeverity.Error ? "red" :
+                severity == ValidationMessageSeverity.Warning ? "yellow" :
+                "skyblue";
 
-			Action title = () => {
-				w.Icon("warning", a => a.Style("margin-right: 4px; color:" + color));
-				w.Write("Предупреждение");
-			};
+            Action title = () => {
+                w.Icon("warning", a => a.Style("margin-right: 4px; color:" + color));
+                w.Write("Предупреждение");
+            };
 
-			w.FieldsBlockCollapsible(title, () => {
-				w.Div(a => a.Class("validation-body").Class("widthstd").GridColumn(Grid.OneWhole), () => {
+            attributes += a => a.Class("validation-body").GridColumn(Grid.OneWhole);
+
+            w.FieldsBlockCollapsible(title, () => {
+                w.Div(attributes, () => {
                     content();
-				});
-			});
-		}
+                });
+            });
+        }
 
-
-		public static LayoutWriter ValidationBlock(this LayoutWriter w, ValidationMessageCollection val)
+        private static LayoutWriter ValidationBlockImpl(this LayoutWriter w, ValidationMessageCollection val, Action<TagAttributes> attributes = null)
         {
             var severity = val.Count == 0 ? ValidationMessageSeverity.Error :
-                val.Any(o => o.Severity == ValidationMessageSeverity.Error) ? ValidationMessageSeverity.Error :
-                val.Any(o => o.Severity == ValidationMessageSeverity.Warning) ? ValidationMessageSeverity.Warning :
-				ValidationMessageSeverity.Information;
+               val.Any(o => o.Severity == ValidationMessageSeverity.Error) ? ValidationMessageSeverity.Error :
+               val.Any(o => o.Severity == ValidationMessageSeverity.Warning) ? ValidationMessageSeverity.Warning :
+               ValidationMessageSeverity.Information;
 
             w.ValidationBlockContainer(severity, () => {
-				foreach (var item in val)
-					w.P(() => w.Write(item.Message));
-			});
+                foreach (var item in val)
+                    w.P(() => w.Write(item.Message));
+            }, attributes);
 
             return w;
         }
