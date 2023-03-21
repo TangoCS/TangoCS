504b38c2e792323bb98e273bfbd1d116a6480c31
Максим Федоров <maks.gm@gmail.com>
2023-03-21 15:13:23 +0300

    tangohub

--- Tango.Client.Js/src/tango/tangohub.js
+++ Tango.Client.Js/src/tango/tangohub.js
@@ -1,24 +1,31 @@
-﻿import { formatPrefix } from "d3";
-
-window.tangohub = function () {
+﻿window.tangohub = function () {
     const connections = {};
     const url = "tangoHub";
+    var prefix;
     var instance = {
         init: function (args) {
+            prefix = args.prefix;
             if (!connections[url]) {
-                var connection = new signalR.HubConnectionBuilder().withUrl("/" + url).build();
+                var connection = new signalR.HubConnectionBuilder()
+                    .withUrl("/" + url)
+                    .configureLogging(signalR.LogLevel.Information)
+                    .build();
                 connections[url] = connection;
 
                 connection.on("SetElementValue", function (id, value) {
-                    if (args.prefix)
-                        id = args.prefix + "_" + id;
+                    if (prefix)
+                        id = prefix + "_" + id;
                     ajaxUtils.setValue({ id: id, value: value });
                 });
                 connection.on("ProcessApiResponse", function (apiResponse) {
                     ajaxUtils.processApiResponse(JSON.parse(apiResponse));
                 });
+                connection.onclose(async () => {
+                    await start(connection);
+                });
 
                 connection.start().then(function () {
+                    console.log("SetServiceAction: " + args.service + "." + args.action + "." + args.key);
                     connection.invoke("SetServiceAction", args.service, args.action, args.key).catch(function (err) {
                         return console.error(err.toString());
                     });
@@ -27,10 +34,21 @@ window.tangohub = function () {
                 });
             }
             else {
+                console.log("SetServiceAction: " + args.service + "." + args.action + "." + args.key);
                 connections[url].invoke("SetServiceAction", args.service, args.action, args.key).catch(function (err) {
                     return console.error(err.toString());
                 });
-			}
+            }
+        }
+    };
+
+    async function start(connection) {
+        try {
+            await connection.start();
+            console.log("SignalR Connected.");
+        } catch (err) {
+            console.log(err);
+            setTimeout(function () { start(connection); }, 5000);
         }
     };
     return instance;
