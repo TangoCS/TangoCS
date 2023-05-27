8a6bb3f86508402a12514d04436ad775a029cc36
Максим Федоров <maks.gm@gmail.com>
2023-05-27 21:23:14 +0300

    ViewPagePart.GetFormID()

---
+++ Tango.UI.Std/UI/FormIDAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tango.UI
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+	public class FormIDAttribute : Attribute
+	{
+		public Guid Guid { get; }
+
+		public FormIDAttribute(Guid guid)
+		{
+			this.Guid = guid;
+		}
+	}
+}
