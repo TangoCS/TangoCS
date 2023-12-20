6aabc58c3e360f30cea6ab7c507d38ec50aae577
Vadim <Vadim@VADIM-HOME-3>
2023-12-20 19:33:54 +0300

    inline editable fields

---
+++ Tango.Client.Js/src/tango/editablefield.js
@@ -0,0 +1,12 @@
+﻿window.Tango.inlineFieldHelper = function (field) {
+	const imp = document.getElementById(field);
+	imp.focus();
+	
+	const bodyClick = function () {
+		const cancelBtn = document.getElementById(field + '_canceledit');
+		ajaxUtils.postEventFromElementWithApiResponse(cancelBtn);
+		document.body.removeEventListener('click', bodyClick);
+	};
+
+	document.body.addEventListener('click', bodyClick);
+};
